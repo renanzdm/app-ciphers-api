d27edea9ff71eee5b3c0d300099bb2d594649ab1
renanzumas <renan.miranda@agrotis.com>
2022-07-05 20:22:55 -0300

    .

--- src/Routes.ts
+++ src/Routes.ts
@@ -20,31 +20,30 @@ routes.route('/signup').post(
       `, {
       "email": email
     }
-    );
+    )
+     if (TBL_USERS.length > 0) {
+       return response.status(402).send({
+         "message": 'Email ja esta cadastrado'
+       });
+     } 
 
-    // if (TBL_USERS.length > 0) {
-    //   return response.status(402).send({
-    //     "message": 'Email ja esta cadastrado'
-    //   });
-    // } 
-
-    // const { insert_TBL_USERS } = await client.request(
-    //   gql`mutation MyMutation($user_email:String!,$user_name:String! $user_password:String!,$user_salt:String!) {
-    //   insert_TBL_USERS(objects: {user_email: $user_email, user_name: $user_name, user_password: $user_password, user_salt: $user_salt}) {
-    //     returning {
-    //       user_id
-    //       user_email
-    //     }
-    //   }
-    // }`,
-    //   {
-    //     "user_email": email, "user_password": hashedPass, "user_salt": saltHash, "user_name": name
-    //   }
-    // );
-    // const { user_id, user_email } = insert_TBL_USERS['returning'][0];
+     const { insert_TBL_USERS } = await client.request(
+       gql`mutation MyMutation($user_email:String!,$user_name:String! $user_password:String!,$user_salt:String!) {
+       insert_TBL_USERS(objects: {user_email: $user_email, user_name: $user_name, user_password: $user_password, user_salt: $user_salt}) {
+         returning {
+           user_id
+           user_email
+         }
+       }
+     }`,
+       {
+         "user_email": email, "user_password": hashedPass, "user_salt": saltHash, "user_name": name
+       }
+     );
+     const { user_id, user_email } = insert_TBL_USERS['returning'][0];
     return response.status(200).send({
-      'id': 'user_id',
-      "email": 'user_email'
+      'id': user_id,
+      "email": user_email
     });
   }
 );
