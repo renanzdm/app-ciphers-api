8374c0ce540fbbd1ed277a1a582d0221487eb37f
renanzumas <renan.miranda@agrotis.com>
2022-07-05 16:40:45 -0300

    .

--- src/Routes.ts
+++ src/Routes.ts
@@ -8,43 +8,43 @@ const routes = Router();
 
 routes.route('/signup').post(
   async (request: Request, response: Response) => {
-    const { email, password, name } = request.body.input;
-    const saltHash = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(password, saltHash);
-    const { TBL_USERS } = await client.request(
-      gql`query MyQuery($email: String!) {
-        TBL_USERS(where: {user_email: {_eq: $email}}) {
-          user_email
-        }
-      }
-      `, {
-      "email": email
-    }
-    );
+    // const { email, password, name } = request.body.input;
+    // const saltHash = await bcrypt.genSalt(10);
+    // const hashedPass = await bcrypt.hash(password, saltHash);
+    // const { TBL_USERS } = await client.request(
+    //   gql`query MyQuery($email: String!) {
+    //     TBL_USERS(where: {user_email: {_eq: $email}}) {
+    //       user_email
+    //     }
+    //   }
+    //   `, {
+    //   "email": email
+    // }
+    // );
 
-    if (TBL_USERS.length > 0) {
-      return response.status(402).send({
-        "message": 'Email ja esta cadastrado'
-      });
-    } 
+    // if (TBL_USERS.length > 0) {
+    //   return response.status(402).send({
+    //     "message": 'Email ja esta cadastrado'
+    //   });
+    // } 
 
-    const { insert_TBL_USERS } = await client.request(
-      gql`mutation MyMutation($user_email:String!,$user_name:String! $user_password:String!,$user_salt:String!) {
-      insert_TBL_USERS(objects: {user_email: $user_email, user_name: $user_name, user_password: $user_password, user_salt: $user_salt}) {
-        returning {
-          user_id
-          user_email
-        }
-      }
-    }`,
-      {
-        "user_email": email, "user_password": hashedPass, "user_salt": saltHash, "user_name": name
-      }
-    );
-    const { user_id, user_email } = insert_TBL_USERS['returning'][0];
+    // const { insert_TBL_USERS } = await client.request(
+    //   gql`mutation MyMutation($user_email:String!,$user_name:String! $user_password:String!,$user_salt:String!) {
+    //   insert_TBL_USERS(objects: {user_email: $user_email, user_name: $user_name, user_password: $user_password, user_salt: $user_salt}) {
+    //     returning {
+    //       user_id
+    //       user_email
+    //     }
+    //   }
+    // }`,
+    //   {
+    //     "user_email": email, "user_password": hashedPass, "user_salt": saltHash, "user_name": name
+    //   }
+    // );
+    // const { user_id, user_email } = insert_TBL_USERS['returning'][0];
     return response.status(200).send({
-      'id': user_id,
-      "email": user_email
+      'id': 'user_id',
+      "email": 'user_email'
     });
   }
 );
